daf0c3ab552d00f03377161ff34ac9dc7384dcda
Matias Nuñez <54615023+MatiasNuniez@users.noreply.github.com>
2020-12-26 09:06:57 -0300

    asdhasd

---
+++ jsbasico.js
@@ -0,0 +1,13 @@
+let persona = {
+    nombre : "matias",
+    apellido : "Nunez",
+    dni : 40790916,
+    email : "matiasnuniez192@gmail.com"
+}
+persona.nombre
+persona.apellido
+persona.dni
+persona.email
+
+// importante en js es window que tiene todas las propiedades de la ventana en la que estamos 
+//document 
